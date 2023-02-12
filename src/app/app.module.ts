f1282508e050683b4c70bdc2765f8f44089a1324
aymanyori <aymanyori00@gmail.com>
2023-02-12 14:08:09 +0200

    FireBase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterModule } from './feature/footer/footer.module';
 import { HNavbarModule } from './feature/h-navbar/h-navbar.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 
 @NgModule({
@@ -17,7 +22,11 @@ import { HNavbarModule } from './feature/h-navbar/h-navbar.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     HNavbarModule,
-    FooterModule
+    FooterModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'bakery-eabce',
+    appId: '1:66435402920:web:67b0748f7ec3cb3f33869d',
+    databaseURL: 'https://bakery-eabce-default-rtdb.firebaseio.com',
+    storageBucket: 'bakery-eabce.appspot.com',
+    apiKey: 'AIzaSyAy0-1xxBCa53ooY0grdY7tMLOy2cUE8l4',
+    authDomain: 'bakery-eabce.firebaseapp.com',
+    messagingSenderId: '66435402920',
+    measurementId: 'G-0RV25233Q3',
+  },
   production: false
 };
 
