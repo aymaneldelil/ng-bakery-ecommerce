93a09190381a429edb6b6fe7640184f4c9bd3fa4
aymanyori <aymanyori00@gmail.com>
2023-02-12 14:19:29 +0200

    "Adding  routing"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./view/public/home/home.module').then((t) => t.HomeModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./view/public/auth/auth.module').then((t) => t.AuthModule),
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
