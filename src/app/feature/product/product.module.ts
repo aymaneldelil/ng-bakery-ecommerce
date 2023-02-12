74d4830a0052393c8480a71ff439f2fd7a9a0de1
aymanyori <aymanyori00@gmail.com>
2023-02-12 22:43:14 +0200

    Product Card Ui

--- src/app/feature/product/product.module.ts
+++ src/app/feature/product/product.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from './components/product-card/product-card.component';
-
+import { AngularMaterialModule } from '../angular-material/angular-material.module';
 
 
 @NgModule({
@@ -9,7 +9,9 @@ import { ProductCardComponent } from './components/product-card/product-card.com
     ProductCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+AngularMaterialModule
+
   ],
   exports:[
     ProductCardComponent
