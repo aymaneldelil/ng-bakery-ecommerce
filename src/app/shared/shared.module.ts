eca5056129ba05d18ad98478c067a017e15802f6
aymanyori <aymanyori00@gmail.com>
2023-02-12 13:15:53 +0200

    Import Angular material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { AngularMaterialModule } from '../feature/angular-material/angular-material.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    AngularMaterialModule
+
   ]
 })
 export class SharedModule { }
