ec45f0718b8d389fcd74518da3ac354f7974c164
aymanyori <aymanyori00@gmail.com>
2023-02-14 18:44:27 +0200

    product feature

--- src/app/feature/product/product.module.ts
+++ src/app/feature/product/product.module.ts
@@ -2,19 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
-
+import { ProductFilterComponent } from './components/product-filter/product-filter.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    ProductCardComponent
+    ProductCardComponent,
+    ProductFilterComponent,
+    ProductDetailsComponent,
   ],
-  imports: [
-    CommonModule,
-AngularMaterialModule
-
-  ],
-  exports:[
-    ProductCardComponent
-  ]
+  imports: [CommonModule, AngularMaterialModule],
+  exports: [ProductCardComponent],
 })
-export class ProductModule { }
+export class ProductModule {}
